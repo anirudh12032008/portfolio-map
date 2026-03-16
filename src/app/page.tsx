3f33ea99806f575436672f359e74c6d157c17044
anirudh12032008 <renudilip19@gmail.com>
2026-03-16 19:59:33 +0530

    added cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Cursor } from "@/components/sections/Cursor";
 import { Grid } from "@/components/sections/Grid";
 import { Hero } from "@/components/sections/Hero";
 import { useState } from "react";
@@ -8,10 +9,11 @@ import { useState } from "react";
 export default function Home() {
   return (
     <div className="relative w-full overflow-hidden ">
+      <Cursor />
       {/* patience my friend */}
-      <section className="justify-center pt-32 relative">
+      <section className="justify-center pt-32 relative min-h-screen ">
 
-      <Grid className="absolute inset-0 z-0" /> 
+      <Grid className="absolute inset-0" /> 
       </section>
       {/* <Hero/> */}
     </div>
---
+++ src/components/sections/Cursor.tsx
@@ -0,0 +1,96 @@
+import { use, useEffect, useState } from "react";
+import { motion, useSpring, useMotionValue } from "framer-motion";
+import clsx from "clsx";
+
+
+export const Cursor = () => {
+    const dX = useMotionValue(-100);
+    const dY = useMotionValue(-100);
+    // ahh need to twek this some day........
+    const rX = useSpring( dX, { stiffness: 300, damping: 30 });
+    const rY = useSpring( dY, { stiffness: 300, damping: 30 });
+
+    // const [active, setActive] = useState(false);
+    const [hovered, setHovered] = useState(false);
+    const [ istouch, setIsTouch] = useState(false);
+
+
+    useEffect(() => {
+        const mq = window.matchMedia("(hover: hover) and (pointer: fine)");
+        setIsTouch(!mq.matches);
+
+        const hc = ( e: MediaQueryListEvent) => setIsTouch(!e.matches);
+        mq.addEventListener("change", hc);
+        return () => mq.removeEventListener("change", hc);
+    }, []); 
+
+
+    useEffect(() => {
+        if (istouch) return;
+
+
+        const move = (e: MouseEvent) => {
+            dX.set(e.clientX - 12);
+            dY.set(e.clientY - 12);
+         const t = e.target as HTMLElement;
+         if (
+            t.tagName.toLowerCase() === "a" ||
+            t.tagName.toLowerCase() === "button" ||
+            t.closest("a") ||
+            t.closest("button") ||
+            t.closest("[data-cursor]") ||
+            t.closest("[data-clickable='true']")
+         ) {
+            setHovered(true);
+         } else {
+            setHovered(false);
+         }
+
+
+        };
+        window.addEventListener("mousemove", move);
+        return () => window.removeEventListener("mousemove", move);
+    }, [dX, dY, istouch]);
+
+    if (istouch) return null;
+
+    const dotC = 'bg-olive-dark';
+    const ringC = hovered ? "border-olive-dark" : "border-olive-light/50";
+
+    return (
+        <>
+        
+        <motion.div
+        className={clsx(
+            "fixed top-0 left-0 pointer-events-none z-50 rounded-full border border-solid transition-colors duration-300",
+            ringC,
+        )}
+        style={{
+            x: rX,
+            y: rY,
+            translateX: "-50%",
+            translateY: "-50%",
+        }}
+        animate={{ width: hovered ? 60 : 30, height: hovered ? 60 : 30, opacity: hovered ? 0.5 : 1   }}
+transition={{
+    type: "spring",
+    stiffness: 300,
+    damping: 25
+}}/>
+
+<motion.div
+className={clsx(
+    "fixed top-0 left-0 w-2 h-2 rounded-full pointer-events-none z-50 transition-colors duration-300",
+    dotC,
+)}  
+style={{
+    x: dX,
+    y: dY,
+    translateX: "-50%",
+    translateY: "-50%",
+
+}}
+/>
+        </>
+    );
+};
